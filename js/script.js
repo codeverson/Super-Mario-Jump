3b6e0eb4f4b572983ac87ca6decfeca5ef1a3258
Everson Araujo <araujoeverson@icloud.com>
2023-05-01 22:17:58 -0300

    som

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const jumpSound = new Audio('./audio/smw_jump.wav'); // criando objeto Audio para o som de pulo
+
 
 const jump = () => {
     mario.classList.add('jump');
-
+    jumpSound.play();
     setTimeout(() => {
       mario.classList.remove('jump');
     }, 500)
@@ -17,17 +19,21 @@ const loop = setInterval(() => {
     console.log(marioPosition);
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
+        const gameOverSound = new Audio('./audio/smw_lost_a_life.wav'); // criando objeto Audio para o som de game over
+        const backgroundMusic = document.querySelector('.background-music');
+        backgroundMusic.pause();
+        gameOverSound.play(); // reproduzindo o som de game over
+        
         pipe.style.animation = 'none'; 
         pipe.style.left = `${pipePosition}px`;
-
+ 
         mario.style.animation = 'none'; 
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './images/game-over.png'
         mario.style.width = '75px'
         mario.style.marginLeft = "50px"
-
+ 
         clearInterval(loop);
     }
  
