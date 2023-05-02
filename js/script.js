765adc98fa47c41552578ce84ec48bf02dc17c6f
Everson Araujo <araujoeverson@icloud.com>
2023-05-01 23:31:09 -0300

    botao working

--- js/script.js
+++ js/script.js
@@ -7,42 +7,35 @@ backgroundMusic.loop = true;
 backgroundMusic.play();
 
 const jump = () => {
-    mario.classList.add('jump');
-    jumpSound.play();
-    setTimeout(() => {
-      mario.classList.remove('jump');
-    }, 500)
+  mario.classList.add('jump');
+  jumpSound.play();
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  }, 500)
 }
 
 const loop = setInterval(() => {
-
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    
-    console.log(marioPosition);
-
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        const gameOverSound = new Audio('./audio/smw_lost_a_life.wav'); // criando objeto Audio para o som de game over
-
-    
-        gameOverSound.play(); // reproduzindo o som de game over
-
-        backgroundMusic.pause();
-
-        pipe.style.animation = 'none'; 
-        pipe.style.left = `${pipePosition}px`;
- 
-        mario.style.animation = 'none'; 
-        mario.style.bottom = `${marioPosition}px`;
-
-        mario.src = './images/game-over.png'
-        mario.style.width = '75px'
-        mario.style.marginLeft = "50px"
- 
-        clearInterval(loop);
-    }
- 
-
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    const gameOverSound = new Audio('./audio/smw_lost_a_life.wav'); // criando objeto Audio para o som de game over
+    gameOverSound.play(); // reproduzindo o som de game over
+    backgroundMusic.pause();
+    pipe.style.animation = 'none'; 
+    pipe.style.left = `${pipePosition}px`;
+    mario.style.animation = 'none';  
+    mario.style.bottom = `${marioPosition}px`;
+    mario.src = './images/game-over.png'
+    mario.style.width = '75px'
+    mario.style.marginLeft = "50px"
+    clearInterval(loop);
+    document.querySelector('.container').style.display = 'block'; // mostra o botão de reiniciar
+  }
 }, 10);
+
+document.querySelector('.restartbutton').addEventListener('click', () => {
+  window.location.reload(); // recarrega a página para reiniciar o jogo
+});
 
 document.addEventListener('keydown', jump);
